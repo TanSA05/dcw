05bf59d86c5d38e665a6b89ae3167a2dc4fff1e1
Ankur Singh <ankur13019@iiitd.ac.in>
2015-10-29 12:49:07 +0530

    added seed data for complaints

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,16 +5,39 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-Organization.create!(name: "DCW", :category => :dcw)
+Faker::Config.locale = 'en-IND'
+
+Organization.create(name: "DCW", :category => :dcw)
 
 user = CreateAdminService.new.call
 puts 'CREATED ADMIN USER: ' << user.email
 # Environment variables (ENV['...']) can be set in the file config/application.yml.
 # See http://railsapps.github.io/rails-environment-variables.html
 
-Organization.create!(name: "Police Branch 1", :category => :police)
-Organization.create!(name: "Police Branch 2", :category => :police)
-Organization.create!(name: "CAW", :category => :others)
-Organization.create!(name: "DLSA", :category => :others)
-Organization.create!(name: "ICC", :category => :others)
-Organization.create!(name: "CAW Division 2", :category => :others)
+organizations = [
+	{name: "Police Branch 1", :category => :police},
+	{name: "Police Branch 2", :category => :police},
+	{name: "CAW", :category => :others},
+	{name: "DLSA", :category => :others},
+	{name: "ICC", :category => :others},
+	{name: "CAW Division 2", :category => :others}
+]
+
+# Organization.create(name: "Police Branch 1", :category => :police)
+# Organization.create(name: "Police Branch 2", :category => :police)
+# Organization.create(name: "CAW", :category => :others)
+# Organization.create(name: "DLSA", :category => :others)
+# Organization.create(name: "ICC", :category => :others)
+# Organization.create(name: "CAW Division 2", :category => :others)
+
+organizations.each do |x|
+	Organization.create(x)
+end
+
+100.times do |x|
+	Complaint.create(complainant: Faker::Name.name,respondent_if_person: Faker::Name.name,
+		contact_number_of_complainant: Faker::PhoneNumber.phone_number, address: Faker::Address.street_address,
+		locality: Complaint.locality.values.sample, brief_of_complaint: Faker::Hacker.say_something_smart,
+		prayers: Faker::Lorem.sentence
+	)
+end
