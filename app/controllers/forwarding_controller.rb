2e5ec7514a92d8b241c8f7473117525d08c86e7c
Ankur Singh <ankur13019@iiitd.ac.in>
2015-11-05 14:57:52 +0530

    added forwarding skeleton

---
+++ app/controllers/forwarding_controller.rb
@@ -0,0 +1,19 @@
+class ForwardingController < ApplicationController
+  def dcw_feedback
+  end
+
+  def cc_feedback
+  end
+
+  def agency_feedback
+  end
+
+  def interim
+  end
+
+  def final
+  end
+
+  def do
+  end
+end
